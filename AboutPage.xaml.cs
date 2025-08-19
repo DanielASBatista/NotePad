e872742b67088f581a5175972fde088553773d9e
Etec Prof. Horácio Augusto da Silveira <discov126@.com>
2025-08-18 22:30:21 -0300

    first commit

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace Notes;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+
+    private void LinkGit_Clicked(object sender, EventArgs e)
+    {
+		Launcher.Default.OpenAsync("https://github.com/fantasyslayer?tab=repositories");
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        String caminho = Path.Combine(FileSystem.AppDataDirectory,"arquivo");
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Apagarbtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Salvarbtn_Clicked(object sender, EventArgs e)
+        {
+            String conteudo = CaixaEditor.Text;
+            File.WriteAllText(caminho , conteudo);
+        }
+    }
+
+}
